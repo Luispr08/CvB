64280f4e7733cc6bc93ae005a9f26fa9de42f73a
Luispr08 <luispr08081994@gmail.com>
2018-11-18 23:50:44 -0500

    Upload
    Update on github

---
+++ Brains & Colors/Assets/Spawner/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Transform Spawnpoint;
+    public GameObject Cube;
+    public KeyCode Ctrl;
+    void Start ()
+    {
+       
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {   if (Input.GetKeyDown(Ctrl))
+        {
+            Instantiate(Cube, Spawnpoint.position, Spawnpoint.rotation);
+            
+        }
+
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# CvB
-Unity Game
