426f4391ddab5c4946e3f868d47663b9abd2a169
Luispr08 <luispr08081994@gmail.com>
2019-01-28 19:25:10 -0500

    progress

--- Brains & Colors/Assets/Col/Dectection.cs
+++ Brains & Colors/Assets/Col/Dectection.cs
@@ -15,14 +15,15 @@ public class Dectection : MonoBehaviour
     {
         
     }
-    void OnCollisionEnter(Collision col)
+    void OnCollisionEnter(Collision col) //If cubes touch identifier cubes and identifier destroy// Here I have to add the reduction of lives if this happens.
     {
         Debug.Log("collision name = " + col.gameObject.name); //I have to specify whith which object should they collide in order to disspear
         if (col.gameObject.name == "BlueIdentifier(Clone)" || col.gameObject.name == "YellowIdentifier(Clone)" || col.gameObject.name == "RedIdentifier(Clone)" || col.gameObject.name == "PinkIdentifier(Clone)" ||
             col.gameObject.name == "OrangeIdentifier(Clone)" ||  col.gameObject.name == "GreenIdentifier(Clone)" || col.gameObject.name == "PurpleIdentifier(Clone)")
         {
-            Destroy(col.gameObject);
-            Destroy(this.gameObject);
+            Destroy(col.gameObject); //This is the name of the identifier therefore is destroyed. "col" that is.
+            Destroy(this.gameObject);//This destroys all the objects that have this script. 
         }
     }
+
 }
---
+++ Brains & Colors/Assets/Tapping/Tap.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        Debug.Log("Object was tapped");
+        //Detect which object got tapped on
+        //Check if the tag matches the identifiers tag.
+        //Do if statements to decide which function to call to either destroy all the objects or just that one object.
+       //public var Tag_checker: GameObject;
+        GameObject.FindGameObjectWithTag(gameObject.tag); //We get the tag of this object
+        
+    }
+
+    void Des_all() //We tapped the correct object destroy all the objects
+    {
+
+    }
+    void Des_one() //We tapped the wrong object destroy only the tapped object.
+    {
+
+    }
+}
