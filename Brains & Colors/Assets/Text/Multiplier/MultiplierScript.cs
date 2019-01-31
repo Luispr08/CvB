df8108c12b91d85f54ae557b807f199aabfe1db4
Luispr08 <luispr08081994@gmail.com>
2019-01-30 23:15:29 -0500

    Added Multiplier
    Added multiplier text and script. Needs Mechanism!

---
+++ Brains & Colors/Assets/Text/Multiplier/MultiplierScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplierScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int multi = 5;
+    TextMesh MultiText;
+    void Start()
+    {
+        MultiText = gameObject.GetComponent("TextMesh") as TextMesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MultiText.text = "X " + multi.ToString();
+    }
+}
