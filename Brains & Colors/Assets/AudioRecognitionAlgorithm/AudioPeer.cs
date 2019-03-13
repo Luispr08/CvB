b909197b87f4090e0bc94e2b2df1a9a260bfc2df
Luispr08 <luispr08081994@gmail.com>
2019-03-13 18:32:22 -0400

    Music visualizaton in progress
    It has been successfull I am now make the cubes scale depending on the beat of the music and they visualize the music I need to work more on this and then transition to particles to see how particles could react with this or how to do it. This is probably gonna be one of the hardest parts with particles as i dont know if it would be with the emission of particles and size of particles all at the same time.

---
+++ Brains & Colors/Assets/AudioRecognitionAlgorithm/AudioPeer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+    AudioSource _audioSource;
+    public static float[] _samples = new float[512];
+
+    public static float[] _freqBand = new float[8];
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrucmAudioSource();
+        MakeFrequencyBands();
+    }
+    void GetSpectrucmAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples,0,FFTWindow.Blackman);
+    }
+
+    void MakeFrequencyBands()
+    {
+        /*
+         * 22050 hertz / 512 = 43hertz per sample
+         * audio can be devided into 7 channels
+         * 20-60 hz
+         * 60-250 hz
+         * 250-500
+         * 500-2000
+         * 2000-4000
+         * 4000 - 6000
+         * 6000 - 20000
+         * 0 -2 = 86hz
+         * 1 -4 = 172 hz  range= 87-258 hz 
+         * 2
+         * 3
+         * 4
+         * 5
+         * 6
+         * 7
+         */
+
+        int count = 0;
+        for (int i = 0; i <8; i++)
+        {
+            int sampleCount = (int)Mathf.Pow(2,i)*2;
+            float average = 0;
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += _samples[count]*(count+1);
+
+                count++;
+            }
+            average /= count;
+
+            _freqBand[i] = average * 10;
+        }
+         
+    }
+}
